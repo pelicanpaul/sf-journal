811ada4775b6014cec58332241b161941f178928
pelicanpaul <thepaullyons@gmail.com>
2019-10-28 07:34:32 -0700

    shadowbox

--- wp-content/themes/sf-journal/header.php
+++ wp-content/themes/sf-journal/header.php
@@ -24,7 +24,7 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/sf-journal/icons/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/sf-journal/icons/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/sf-journal/icons/favicon-16x16.png">
-	<link rel="manifest" href="/wp-content/themes/sf-journal/icons/site.webmanifest">
+	<link rel="manifest" href="https://sfjournal.net//wp-content/themes/sf-journal/icons/site.webmanifest">
 	<link rel="mask-icon" href="/wp-content/themes/sf-journal/icons/safari-pinned-tab.svg" color="#cd2b2b">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#ffffff">
