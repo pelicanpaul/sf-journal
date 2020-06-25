03beeecee20fc850476b0e5cfb3cd9f205a0e359
pelicanpaul <thepaullyons@gmail.com>
2020-06-25 12:37:48 -0700

    search in top nav

---
+++ wp-content/themes/sf-journal/template-parts/navigation/navigation-top.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentyseventeen' ); ?>">
+	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
+		<?php
+		echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
+		echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
+		_e( 'Menu', 'twentyseventeen' );
+		?>
+	</button>
+
+	<?php wp_nav_menu( array(
+		'theme_location' => 'top',
+		'menu_id'        => 'top-menu',
+	) ); ?>
+
+
+	<div class="container-search-form-top">
+		<form role="search" method="get" class="search-form-top" action="/">
+
+			<input type="search" id="search-form-top" class="search-field" placeholder="Search" value="" name="s">
+			<button type="submit" class="search-submit"><svg class="icon icon-search" aria-hidden="true" > <use href="#icon-search" xlink:href="#icon-search"></use> </svg><span class="screen-reader-text">Search</span></button>
+		</form>
+	</div>
+</nav><!-- #site-navigation -->
